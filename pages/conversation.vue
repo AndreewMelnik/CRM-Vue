3d13b485337f42df94f5354add1a9e5760bb8973
Andrew Melnik <andreew.mel@gmail.com>
2023-01-12 20:18:53 +0300

    template/icons/vuetify - working need to make - components, pagination, authorization,charts, google-sheets adding

---
+++ pages/conversation.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "conversation.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/marketing.vue
@@ -0,0 +1,13 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: "marketing.vue"
+}
+</script>
+
+<style scoped>
+
+</style>
