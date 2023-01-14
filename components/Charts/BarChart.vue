df812198e2c4356a352af1347a4043049ed877dd
Andrew Melnik <andreew.mel@gmail.com>
2023-01-14 20:39:47 +0300

    charts added, need to add authorization, google sheets

---
+++ components/Charts/BarChart.vue
@@ -0,0 +1,41 @@
+<template>
+  <Bar :data="chartData"/>
+</template>
+
+<script>
+import {Bar} from 'vue-chartjs'
+import {BarElement, CategoryScale, Chart as ChartJS, Legend, LinearScale, Title, Tooltip} from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: {Bar},
+  data() {
+    return {
+      chartData: {
+        labels: [
+          'January',
+          'February',
+          'March',
+          'April',
+          'May',
+          'June',
+          'July',
+          'August',
+          'September',
+          'October',
+          'November',
+          'December'],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: '#f87979',
+            data: [40, 20, 12, 39, 10, 40, 39, 80, 40, 20, 12, 11]
+          }
+        ]
+      }
+    }
+  }
+}
+</script>
--- pages/sales.vue
+++ pages/sales.vue
@@ -1,10 +1,28 @@
 <template>
+<v-container>
+  <v-row>
+    <v-col cols="12" sm="4">
+      <BarChart/>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <PieChart/>
+    </v-col>
+    <v-col cols="12" sm="4">
+      <LineChart/>
+    </v-col>
+  </v-row>
+</v-container>
 
 </template>
 
 <script>
+import BarChart from "../components/Charts/BarChart";
+import PieChart from "../components/Charts/PieChart";
+import LineChart from "../components/Charts/LineChart";
 export default {
-  name: "sales"
+  name: "sales",
+  components: {PieChart, BarChart, LineChart}
+
 }
 </script>
 
